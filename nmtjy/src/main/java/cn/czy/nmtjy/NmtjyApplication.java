61be8bd0dac66aa9092adc1b2f854cfb65c7fb29
peterchen <peterchenzhy@aliyun.com>
2018-12-23 21:32:17 +0800

    原始版本

---
+++ nmtjy/src/main/java/cn/czy/nmtjy/NmtjyApplication.java
@@ -0,0 +1,14 @@
+package cn.czy.nmtjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmtjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmtjyApplication.class, args);
+    }
+
+}
+
